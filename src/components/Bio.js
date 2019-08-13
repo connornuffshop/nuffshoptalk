f921682c56e4a204a6a8abf89f9d2cc3dec971bf
Connor Bradley <connorbradley@Connors-MacBook-Air-3.local>
2019-08-12 22:20:02 -0400

    bio update

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -25,7 +25,7 @@ class Bio extends React.Component {
         <p style={{ maxWidth: 310 }}>
           Personal blog by{' '}
           <a href="https://twitter.com/_connorb">Connor Bradley</a>.{' '}
-          I&nbsp; thoughts on navigating growth in the world of B2B SaaS
+          I&nbsp; Navigating growth. Thoughts are my own. 
         </p>
       </div>
     );
